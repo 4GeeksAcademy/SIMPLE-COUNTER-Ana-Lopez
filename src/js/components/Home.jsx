6043112d3b8efecd42f1c967b503a347b16d845f
Ana Lopez <avantadesignlab@gmail.com>
2026-03-19 04:04:38 +0000

    style: add styles to counter

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,30 +6,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = (props) => {
 	return (
-		<div className="text-center bg-dark">
+		<div className="container text-center bg-dark mt-5 p-5">
 
 			<h1>Simple Counter</h1>
 
-			<div className="row">
-				<div className="col">
-					<i className="fa-regular fa-clock"></i>
+			<div className="row mt-3">
+				<div className="col rounded-1 border border-secondary-subtle p-3">
+					<i className="fa-regular fa-clock text-white"></i>
 				</div>
-				<div className="col">
+				<div className="col rounded-1 border border-secondary-subtle p-3">
 					{props.digSix}
 				</div>
-				<div className="col">
+				<div className="col rounded-1 border border-secondary-subtle p-3">
 					{props.digFive}
 				</div>
-				<div className="col">
+				<div className="col rounded-1 border border-secondary-subtle p-3">
 					{props.digFour}
 				</div>
-				<div className="col">
+				<div className="col rounded-1 border border-secondary-subtle p-3">
 					{props.digThree}
 				</div>
-				<div className="col">
+				<div className="col rounded-1 border border-secondary-subtle p-3">
 					{props.digTwo}
 				</div>
-				<div className="col">
+				<div className="col rounded-1 border border-secondary-subtle p-3">
 					{props.digOne}
 				</div>
 			
