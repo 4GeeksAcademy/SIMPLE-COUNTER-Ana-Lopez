4a35924d06401996b1127d916c3d2a6381647c3b
Ana Lopez <avantadesignlab@gmail.com>
2026-03-19 14:49:10 +0000

    style:update font, color and text size

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,30 +6,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = (props) => {
 	return (
-		<div className="container text-center bg-dark mt-5 p-5">
+		<div className="container text-center bg-dark mt-5 p-5 text-white">
 
 			<h1>Simple Counter</h1>
 
 			<div className="row mt-3">
-				<div className="col rounded-1 border border-secondary-subtle p-3">
+				<div className="col counter-box rounded text-white p-3 m-2">
 					<i className="fa-regular fa-clock text-white"></i>
 				</div>
-				<div className="col rounded-1 border border-secondary-subtle p-3">
+				<div className="col counter-box rounded text-white p-3 m-2">
 					{props.digSix}
 				</div>
-				<div className="col rounded-1 border border-secondary-subtle p-3">
+				<div className="col counter-box rounded text-white p-3 m-2">
 					{props.digFive}
 				</div>
-				<div className="col rounded-1 border border-secondary-subtle p-3">
+				<div className="col counter-box rounded text-white p-3 m-2">
 					{props.digFour}
 				</div>
-				<div className="col rounded-1 border border-secondary-subtle p-3">
+				<div className="col counter-box rounded text-white p-3 m-2">
 					{props.digThree}
 				</div>
-				<div className="col rounded-1 border border-secondary-subtle p-3">
+				<div className="col counter-box rounded text-white p-3 m-2">
 					{props.digTwo}
 				</div>
-				<div className="col rounded-1 border border-secondary-subtle p-3">
+				<div className="col counter-box rounded text-white p-3 m-2">
 					{props.digOne}
 				</div>
 			
